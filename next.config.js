f08aed479c2b7d7de8dbfa702e908d6673ac8334
Pornsawan Kantirangsima <mushroom.gead@gmail.com>
2023-03-07 09:29:31 +0700

    init commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en-US", "th-TH"],
+    defaultLocale: "th-TH",
+  },
+};
+
+module.exports = nextConfig;
